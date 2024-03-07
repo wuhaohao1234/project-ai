26148d0868407503468d7c40fa20743caa2e0579
wuhaohao <1611499758@qq.com>
2024-03-07 19:13:59 +0800

    fix: port

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.post('/uploadText', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
